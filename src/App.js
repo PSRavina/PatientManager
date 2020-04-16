ab8d530c95687ef58764ac3ec57d854d9a741311
PSRavina <pedrosanchezravina@gmail.com>
2020-04-17 00:14:42 +0100

    avanzando

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import PatientList from "./patientList";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Formulario from "./components/Formulario";
 
 function App() {
+
+  // creamos un state con un arreglo de citas, que inicia vacío
+  const[citas, guardarCitas]=useState([]);
+
+//Creamos una función que coja las tareas actuales más la añadida
+const crearCita=cita=>{
+
+  guardarCitas([
+    ...citas, //cogemos todas las citas que haya, clonando el array, porque si no lo sobreescribimos.
+    cita //le añadimos la cita nueva
+  ])
+//Reiniciar el formulario
+}
+
   return (
     <Fragment>
       <h1>Administración de pacientes</h1>
       <div className="container">
         <div className="row">
           <div className="one-half column">
-            1<Formulario></Formulario>
+            <Formulario
+            crearCita={crearCita}></Formulario>
           </div>
 
           <div className="one-half column">
-            2<Formulario></Formulario>
+            <Formulario></Formulario>
           </div>
         </div>
       </div>
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,15 +1,123 @@
-import React, { Fragment } from "react";
-const Formulario = () => {
+import React, { Fragment, useState } from "react";
+import uuid from "uuid/v4";
+//primer state
+const Formulario = ({ crearCita }) => {
+  const [cita, actualizarCita] = useState({
+    mascota: "",
+    propietario: "",
+    fecha: "",
+    hora: "",
+    sintomas: "",
+  });
+
+  //segundo state
+  const [error, actualizarError] = useState(false);
+
+  //actualizar state con valores del formulario
+
+  const actualizarState = (e) => {
+    actualizarCita({
+      ...cita, //hacemos esto para copiar el state y conseguir tener todos los valores de los campos y no reescribirlo uno encima de otro
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  //extraer valores ( destructuring) para no poner todo el rato cita.mascota, solo mascota y con los demas igual
+  const { mascota, propietario, fecha, hora, sintomas } = cita; //sacamos los valores de cita
+
+  // función del botón de crear cita
+
+  const submitCita = (e) => {
+    e.preventDefault();
+
+    //validacion
+    if (
+      mascota.trim() === "" ||
+      propietario.trim() === "" ||
+      fecha.trim() === "" ||
+      hora.trim() === "" ||
+      sintomas.trim() === ""
+    ) {
+      actualizarError(true);
+      return;
+    }
+    //eliminar mensaje de error
+    actualizarError(false);
+
+    //Asignar ID única (hemos instalado npmi uui que genera ids)
+    //para añadir un valor de id simplemente accedemos a el :
+    cita.id = uuid();
+
+    //Crear cita
+
+    crearCita(cita);
+
+    //Reiniciar el Formulario
+
+    actualizarCita({
+      mascota: "",
+      propietario: "",
+      fecha: "",
+      hora: "",
+      sintomas: "",
+    });
+  };
+
   return (
     <Fragment>
-      <form>
-        <label>Nombre dueño</label>
+      <h2>Crear Cita</h2>
+      {error ? (
+        <p className="alerta-error">Todos los campos son obligatorios</p>
+      ) : null}
+      <form onSubmit={submitCita}>
+        <label>Nombre Mascota</label>
         <input
           type="text"
           name="mascota"
           className="u-full-width"
           placeholder="Nombre de Mascota"
+          onChange={actualizarState}
+          value={mascota}
         />
+
+        <label>Nombre dueño</label>
+        <input
+          type="text"
+          name="propietario" //mismo que en el useState de arriba en todos los inputs
+          className="u-full-width"
+          placeholder="Nombre de Dueño"
+          onChange={actualizarState}
+          value={propietario}
+        />
+
+        <label>Fecha</label>
+        <input
+          type="date"
+          name="fecha"
+          className="u-full-width"
+          onChange={actualizarState}
+          value={fecha}
+        />
+
+        <label>Hora</label>
+        <input
+          type="time"
+          name="hora"
+          className="u-full-width"
+          onChange={actualizarState}
+          value={hora}
+        />
+
+        <label>Síntomas</label>
+        <textarea
+          className="u-full-width"
+          name="sintomas"
+          onChange={actualizarState}
+          value={sintomas}
+        ></textarea>
+        <button type="submit" className="u-full-width button-primary">
+          Agregar Cita
+        </button>
       </form>
     </Fragment>
   );
