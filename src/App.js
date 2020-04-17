141151d1f0bee93aea31472949accba021841a4a
PSRavina <pedrosanchezravina@gmail.com>
2020-04-17 01:00:47 +0100

    añadido las citas

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import React, { Fragment, useState } from "react";
-import PatientList from "./patientList";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Formulario from "./components/Formulario";
+import React, { Fragment, useState, useEffect } from 'react';
+import Formulario from './components/Formulario';
+import Cita from './components/Cita';
 
 function App() {
-
   // creamos un state con un arreglo de citas, que inicia vacío
-  const[citas, guardarCitas]=useState([]);
+  const [citas, guardarCitas] = useState([]);
 
-//Creamos una función que coja las tareas actuales más la añadida
-const crearCita=cita=>{
-
-  guardarCitas([
-    ...citas, //cogemos todas las citas que haya, clonando el array, porque si no lo sobreescribimos.
-    cita //le añadimos la cita nueva
-  ])
-//Reiniciar el formulario
-}
+  //Creamos una función que coja las tareas actuales más la añadida
+  const crearCita = (cita) => {
+    guardarCitas([
+      ...citas, //cogemos todas las citas que haya, clonando el array, porque si no lo sobreescribimos.
+      cita, //le añadimos la cita nueva
+    ]);
+  };
 
   return (
     <Fragment>
@@ -24,12 +20,17 @@ const crearCita=cita=>{
       <div className="container">
         <div className="row">
           <div className="one-half column">
-            <Formulario
-            crearCita={crearCita}></Formulario>
+            <Formulario crearCita={crearCita}></Formulario>
           </div>
 
           <div className="one-half column">
-            <Formulario></Formulario>
+            <h2>Administra tus citas</h2>
+            {citas.map(cita => (
+              <Cita
+              key={cita.id}
+              cita={cita} 
+              />
+            ))}
           </div>
         </div>
       </div>
